54a87fa4cf0f1db788ac26d07434ff06c6f4083e
MegLip <70174916+MegLip@users.noreply.github.com>
2020-11-20 02:09:39 +0100

    Update blog.py

--- blog/blog.py
+++ blog/blog.py
@@ -1,2 +1,19 @@
-from __init__ import app
+from flask import Flask
+from blog.models import Entry, db
+from faker import Faker
 
+
+app = Flask(__name__)
+
+
+def generate_entries(how_many=3):
+    fake = Faker()
+
+    for i in range(how_many):
+        post = Entry(
+            title=fake.sentence(),
+            body='\n'.join(fake.paragraphs(15)),
+            is_published=True
+        )
+        db.session.add(post)
+    db.session.commit()
