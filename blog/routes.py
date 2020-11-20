1b2a0bbae9df39010ac01cbeb540546b409436d1
MegLip <70174916+MegLip@users.noreply.github.com>
2020-11-20 02:08:07 +0100

    Update routes.py

--- blog/routes.py
+++ blog/routes.py
@@ -1,9 +1,50 @@
-from flask import render_template
+from flask import render_template, request
 from blog import app
-from blog.models import Entry
+from blog.models import Entry, db
+from blog.forms import EntryForm
 
 
 @app.route("/")
 def index():
-    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    all_posts = Entry.query.filter_by(
+        is_published=True).order_by(Entry.pub_date.desc())
     return render_template("homepage.html", all_posts=all_posts)
+
+
+def create_update(entry_id=None):
+    errors = None
+    if entry_id is not None:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                form.populate_obj(entry)
+                db.session.commit()
+            else:
+                errors = form.errors
+    else:
+        form = EntryForm()
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                entry = Entry(
+                    title=form.title.data,
+                    body=form.body.data,
+                    is_published=form.is_published.data
+                )
+                db.session.add(entry)
+                db.session.commit()
+            else:
+                errors = form.errors
+    return form, errors
+
+
+@app.route("/new-post/", methods=["GET", "POST"])
+def create_entry():
+    form, errors = create_update()
+    return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+def edit_entry(entry_id):
+    form, errors = create_update(entry_id)
+    return render_template("entry_form.html", form=form, errors=errors)
