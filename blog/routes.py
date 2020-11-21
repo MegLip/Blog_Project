3491f2ee9cdd2f864be33a3a88fe5753bbbfe340
MegLip <70174916+MegLip@users.noreply.github.com>
2020-11-21 01:04:43 +0100

    Update routes.py

--- blog/routes.py
+++ blog/routes.py
@@ -1,5 +1,5 @@
-from flask import render_template, request
-from blog import app
+from flask import render_template, request, url_for, redirect, flash
+from blog import db, app
 from blog.models import Entry, db
 from blog.forms import EntryForm
 
@@ -13,16 +13,7 @@ def index():
 
 def create_update(entry_id=None):
     errors = None
-    if entry_id is not None:
-        entry = Entry.query.filter_by(id=entry_id).first_or_404()
-        form = EntryForm(obj=entry)
-        if request.method == 'POST':
-            if form.validate_on_submit():
-                form.populate_obj(entry)
-                db.session.commit()
-            else:
-                errors = form.errors
-    else:
+    if entry_id is None:
         form = EntryForm()
         if request.method == 'POST':
             if form.validate_on_submit():
@@ -33,18 +24,29 @@ def create_update(entry_id=None):
                 )
                 db.session.add(entry)
                 db.session.commit()
+                flash('Post został pomyślnie dodany!')
+                return redirect(url_for("index"))
             else:
                 errors = form.errors
-    return form, errors
+    elif entry_id != 0:
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                form.populate_obj(entry)
+                db.session.commit()
+                flash('Post został pomyślnie zmieniony!')
+                return redirect(url_for("index"))
+            else:
+                errors = form.errors
+    return render_template("entry_form.html", form=form, errors=errors)
 
 
 @app.route("/new-post/", methods=["GET", "POST"])
 def create_entry():
-    form, errors = create_update()
-    return render_template("entry_form.html", form=form, errors=errors)
+    return create_update(entry_id=None)
 
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
 def edit_entry(entry_id):
-    form, errors = create_update(entry_id)
-    return render_template("entry_form.html", form=form, errors=errors)
+    return create_update(entry_id)
