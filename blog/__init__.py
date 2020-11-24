00e6c29e6db70648f4d312954bd2b9099893f83a
MegLip <70174916+MegLip@users.noreply.github.com>
2020-11-24 01:50:02 +0100

    directly set the config variables app.config

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,9 +2,14 @@ from flask import Flask # z 13.4
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
-app.config.from_object(Config)
+#app.config.from_object(Config)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY") or "welcome to the jungle"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') or \
+            'sqlite:///' + 'C://projects/GitTutorial/Blog_Project//blog.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
